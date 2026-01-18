34ff827a02613bc03337b358dfa4511ea98aa8f3
Bartosz Bryniarski <brylka@jelenia.com>
2026-01-18 14:05:10 +0100

    Niedziela

--- test_health.py
+++ test_health.py
@@ -10,4 +10,4 @@ def client():
 def test_health_returns_ok(client):
     response = client.get('/health')
     assert response.status_code == 200
-    assert response.json["status"] == "ok"
+    assert response.json["status"] == "wszystko ok"
